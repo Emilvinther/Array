20493c78bbe7eb7d1e08a2f1fc33b10f3c6c000b
Emil Vinther Lorentzen <emil380e@zbc.dk>
2022-03-22 12:51:24 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] number = new int [9];
+
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = i+1;
+    if (i == 5)
+    {
+        Console.WriteLine(number[i]);
+        number[i] = 12;
+        Console.WriteLine(number[i]);
+    }
+}
+
+//Console.WriteLine (number[0]);
+
+Console.ReadKey();
